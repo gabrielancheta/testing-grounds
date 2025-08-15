2de93da8c7235bd143046f5305d5c57f86e11b25
gabrielancheta <gabriel.ancheta@pasi.ph>
2025-08-15 15:46:10 +0800

    Add files via upload

---
+++ Test 01/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_01
+{
+    public class Class1
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello world");
+        }
+    }
+}
